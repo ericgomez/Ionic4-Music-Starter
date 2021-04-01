b4375fc1892f45f8b26c26f300823ad03e312601
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-04-01 12:44:02 -0600

    Creando una nueva pagina intro y moviendo nuestro contenido de home
    - ionic generate page intro

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,32 +7,6 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  slideOpts = {
-    initialSlide: 0,
-    slidesPerView: 1, // un slide po vista
-    centeredSlides: true, // centrado
-    speed: 400 // Velocidad de trancicion
-  };
-
-  slides = [{
-    title: "Escucha tu música",
-    subTitle: "EN CUALQUIER LUGAR",
-    description: "Los mejores álbumes, las mejores canciones. Escucha y comparte en cualquier momento, a todas horas.",
-    icon: "play",
-  }, 
-  {
-    title: "Disfruta de nuestro reproductor",
-    subTitle: "DE VIDEOS INCREÍBLES",
-    description: "Entra al modo video de nuestro reproductor y obtén acceso a clips, documentales y making offs incríbles de tu artista favorito.",
-    icon: "videocam",
-  }, 
-  {
-    title: "Accede al exclusivo",
-    subTitle: "MODO DEPORTE",
-    description: "Crea una playlist basada en tu actividad física. <br /> Ten reportes y acceso a lo que necesites, integrado ccon GPS!",
-    icon: "bicycle",
-  }]
-
   constructor() {}
 
 }
