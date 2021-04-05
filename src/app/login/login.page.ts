4d424d20e40c700135b0d8aa59004a45b30ed318
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-04-05 12:56:44 -0500

    Validar inputs del formulario de login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -14,6 +14,27 @@ import {
 export class LoginPage implements OnInit {
   loginForm: FormGroup;
 
+  validationMessages = {
+    email: [
+      {
+        // Cuando el error sea de tipo requerido
+        type: 'required', message: 'El email es requerido'
+      },
+      {
+        // Cuando el error no cumple el patron
+        type: 'pattern', message: 'El email es incorrecto'
+      }],
+    password: [
+      {
+        // Cuando el error sea de tipo requerido
+        type: 'required', message: 'El password es requerido'
+      },
+      {
+        // Cuando el error no cumple el patron
+        type: 'minlength', message: 'Minimo 5 letras para el password'
+      }]
+  };
+
   constructor(private formBuilder: FormBuilder) {
     this.loginForm = this.formBuilder.group({
       email: new FormControl(
@@ -21,6 +42,13 @@ export class LoginPage implements OnInit {
         Validators.compose([
           Validators.required,
           Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$'),
+        ])
+      ),
+      password: new FormControl(
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(5),
         ])
       ),
     });
