8e8737fd49de09ef652cc47c8b8e1ade861b3487
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-04-06 17:20:41 -0500

    Uso de componente Slides y opciones avanzadas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,16 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  artists = [{},{},{},{},{}];
+
+  // Le inidcamos al slider por medio de las [options] las caracteriticas que va a tener
+  slideOps = {
+    initialSlide: 2, //slide inicial 
+    slidesPerView: 4, //slide por vista
+    centeredSlides: true, // Centrar los slides
+    speed: 400 // Velocidad de transicion
+  };
+
   constructor() {}
 
 }
