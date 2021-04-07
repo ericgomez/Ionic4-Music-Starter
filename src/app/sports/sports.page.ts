51bac3e0ec0e7b3ebb6621a739f7b5470e927666
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-04-07 17:26:29 -0500

    Crear la página Sport agregando Marcadores, Polilineas

--- src/app/sports/sports.page.ts
+++ src/app/sports/sports.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Plugins } from '@capacitor/core'
+const { Geolocation } = Plugins
 
 @Component({
   selector: 'app-sports',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SportsPage implements OnInit {
 
+  currentCenter: any;
+  coordinates: any[] = [];
+  defaultZoom = 14
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ionViewDidEnter() {
+    this.getCurrentPosition()
+    this.watchPosition()
+  }
+
+  // Obtenemos la primera posicion
+  async getCurrentPosition() {
+    const coordinates = await Geolocation.getCurrentPosition()
+    this.currentCenter = {
+      lat: coordinates.coords.latitude,
+      lng: coordinates.coords.longitude
+    }
+  }
+
+  //Optenemos la posicion en cada que se actualiza
+  watchPosition() {
+    Geolocation.watchPosition({}, position => {
+      this.currentCenter = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      }
+
+      this.coordinates.push({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      })
+    })
   }
 
 }
